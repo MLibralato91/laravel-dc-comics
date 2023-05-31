32cfd82373773ef77cf260fb0e06792dd0847192
MLibralato91 <michelelibralato911@gmail.com>
2023-05-31 15:23:15 +0200

    create request

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,18 +2,24 @@
 
 @section('content')
     <section class="container">
-        <h1>{{ $comic->title }}</h1>
-        <div class="card">
-            <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
-            <div class="card-body">
-                <h5 class="card-series"> {{ $comic->title }}</h5>
-                <p class="card-text">{!! $comic->description !!}</p>
-                <p class="card-text"><strong>Prezzo:</strong>
-                    {{ $comic->price }} - <strong>Tipologia:</strong> {{ $comic->type }} - <strong>Data di uscita:</strong>
-                    {{ $comic->sale_date }}</p>
+        <div class="row">
+            <div class="col-4">
+                <h1>{{ $comic->title }}</h1>
+                <div class="card">
+                    <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
+                    <div class="card-body">
+                        <h5 class="card-series"> {{ $comic->title }}</h5>
+                        <p class="card-text">{!! $comic->description !!}</p>
+                        <p class="card-text"><strong>Prezzo:</strong>
+                            {{ $comic->price }} - <strong>Tipologia:</strong> {{ $comic->type }} - <strong>Data di
+                                uscita:</strong>
+                            {{ $comic->sale_date }}</p>
 
+                    </div>
+                </div>
+                <a href="{{ route('homepage') }}" class="btn btn-primary">Torna alla Home</a>
             </div>
         </div>
-        <a href="{{ route('homepage') }}" class="btn btn-primary">Torna alla Home</a>
+
     </section>
 @endsection
