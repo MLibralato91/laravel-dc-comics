c38f19233ba1b9ed7b268e5a662224454982cdea
MLibralato91 <michelelibralato911@gmail.com>
2023-05-31 15:55:23 +0200

    update all errors in edit.blade

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -20,32 +20,62 @@
             <textarea name="description" id="description" cols="30" rows="10">
                 {{ $comic->description }}
             </textarea>
+            @error('description')
+                <div class="alert alert-danger">
+                    {{ $message }}
+                </div>
+            @enderror
             <textarea name="thumb" id="thumb" cols="30" rows="10">
                 {{ $comic->thumb }}
             </textarea>
+            @error('thumb')
+                <div class="alert alert-danger">
+                    {{ $message }}
+                </div>
+            @enderror
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
                 <input type="text" class="form-control" name="price" id="price" aria-describedby="myPrice"
                     value="{{ $comic->price }}">
                 <div id="myPrice" class="form-text">Inserisci un prezzo</div>
+                @error('price')
+                    <div class="alert alert-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="series" class="form-label">Serie</label>
                 <input type="text" class="form-control" name="series" id="series" aria-describedby="mySeries"
                     value="{{ $comic->series }}">
                 <div id="mySeries" class="form-text">Inserisci la serie</div>
+                @error('series')
+                    <div class="alert alert-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Data</label>
                 <input type="text" class="form-control" name="sale_date" id="sale_date" aria-describedby="mySale_date"
                     value="{{ $comic->sale_date }}">
                 <div id="mySale_date" class="form-text">Inserisci la data (AAAA/MM/DD)</div>
+                @error('sale_date')
+                    <div class="alert alert-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="type" class="form-label">Tipologia</label>
                 <input type="text" class="form-control" name="type" id="type" aria-describedby="myType"
                     value="{{ $comic->type }}">
                 <div id="myType" class="form-text">Inserisci la tipologia</div>
+                @error('type')
+                    <div class="alert alert-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
 
